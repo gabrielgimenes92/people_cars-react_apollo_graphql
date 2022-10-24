0e72e8df9974bbb27fbd28fd1d29cbe0e367310b
Gabriel Gimenes <gabrielcgimenes@gmail.com>
2022-10-24 00:05:17 -0700

    Adds folder structure and starting files to client

---
+++ client/src/components/cards/ComboCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComboCard = () => {
+  return <div></div>;
+};
+
+export default ComboCard;
---
+++ client/src/components/cards/PersonCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PersonCard = () => {
+  return <div></div>;
+};
+
+export default PersonCard;
